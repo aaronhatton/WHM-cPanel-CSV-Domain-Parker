29dc907749827ceb9ad787b08d234808fe81855d
Aaron Hatton <me@aaronhatton.co.uk>
2015-10-27 11:23:03 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# WHM-cPanel-CSV-Domain-Parker
-Park multiple domains to user accounts with a simple CSV file
+# WHM/cPanel park domains to multiple accounts
+
+This script allows you to park multiple domains to multiple accounts with a CSV file, please note that it has everything I need for my accounts and to add/remove please refer to the API documentation (https://documentation.cpanel.net/display/SDK/cPanel+API+2+Functions+-+Park%3A%3Apark)
+
+Please Note: You require to have the ability to login to user's account with your root password (https://documentation.cpanel.net/display/ALD/Log+in+to+a+User's+Account+with+the+Root+Password)
+
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+ 
+	ini_set('max_execution_time', 90000); //300 seconds = 5 minutes
+	$rootpassword = 'CHANGEME';    /*  <== put in the root password for WHM which also works for cpanel for any user */
+	
+	$accountlist_file = file_get_contents("accounts.csv");
+
+	foreach (explode("\r",$accountlist_file) as $csv_row) {
+
+		$csv_row = str_getcsv($csv_row);
+				
+		$user = $csv_row[0];
+		$domain = $csv_row[1];
+		$server = $csv_row[2];
+		$mxcheck = $csv_row[3];
+		
+
+		$user = preg_replace('/\s+/', '', $user);
+		$domain = preg_replace('/\s+/', '', $domain);
+		$server = preg_replace('/\s+/', '', $server);
+		$mxcheck = preg_replace('/\s+/', '', $mxcheck);
+		
+		// Domain Park Query
+		$domain_park = 'https://' . $server . ':2083/json-api/cpanel?cpanel_jsonapi_user=' . $user . '&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Park&cpanel_jsonapi_func=park&domain=' . $domain;
+		
+		// Domain Mail Flow Query
+		$domain_mxset = 'https://' . $server . ':2083/json-api/cpanel?cpanel_jsonapi_user=' . $user . '&cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Email&cpanel_jsonapi_func=setmxcheck&domain=' . $domain . '&mxcheck=' . $mxcheck;
+		
+		$curl = curl_init();       
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);  
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
+		curl_setopt($curl, CURLOPT_HEADER,0);          
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);  
+		$header[0] = "Authorization: Basic " . 
+		base64_encode($user.":".$rootpassword) . "\n\r";
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $header); 
+		
+		// Domain Parker
+		curl_setopt($curl, CURLOPT_URL, $domain_park); 
+		$domain_park_result = curl_exec($curl);
+		if ($domain_park_result == false) {
+			error_log("curl_exec threw error \"" . curl_error($curl) . "\" for $query<br /><br />"); 
+															// log error if curl exec fails
+		}
+		
+		// Domain Email Set MX Check
+		curl_setopt($curl, CURLOPT_URL, $domain_mxset); 
+		$domain_mxset_result = curl_exec($curl);
+		if ($domain_mxset_result == false) {
+			error_log("curl_exec threw error \"" . curl_error($curl) . "\" for $query<br /><br />"); 
+															// log error if curl exec fails
+		}
+
+		curl_close($curl);
+		
+		echo "<h3>" . $domain . " parking attempt</h3>";
+		echo "Attempted to park the domain: " . $domain . " by referencing the username " . $user . " on the server " . $server . "<br /><br />";
+		print $domain_park_result;
+		
+		echo "<br /><br />";
+		
+		echo "Attempted to set the correct mail flow settings for the domain: " . $domain . " on the server " . $server . "<br /><br />";
+		print $domain_park_result;
+		
+		echo "<br /><hr /><br />";
+		
+	}
+ 
+?>
